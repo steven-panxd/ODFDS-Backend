ae8681e0576cebb868eeb12bd15e7ce0855b2442
Steven Pan <steven.panxd@gmail.com>
2023-03-11 17:02:18 -0800

    update "--accept-data-loss"

--- app/start.sh
+++ app/start.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
 until nc -z -v -w30 ${MYSQL_DB_HOST} ${MYSQL_DB_DOCKER_PORT}
 do
   echo "Waiting a second until the database is receiving connections..."
   # wait for a second before checking again
   sleep 1
 done
-npx prisma db push
+npx prisma db push --accept-data-loss
 npm run dev
